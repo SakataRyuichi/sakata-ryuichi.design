d72b3d8221a347783882f8b728e9e3653111e588
SakataRyuichi <ryuichi.design@gmail.com>
2019-03-23 17:53:19 +0900

    Add New Layouts twoColumn

---
+++ sakata-ryuichi-web/layouts/twoColumn.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <GlobalHeader />
+    <section class="container is-fluid">
+      <div class="columns ">
+        <div class="column is-one-third">
+          <SideMenu />
+        </div>
+        <div class="column">
+          <nuxt />
+        </div>
+      </div>
+    </section>
+    <GlobalFooter />
+  </div>
+</template>
+
+
+<script>
+import GlobalHeader from '~/components/GlobalHeader.vue'
+import GlobalFooter from '~/components/GlobalFooter.vue'
+import SideMenu from '~/components/SideMenu.vue'
+
+export default {
+  components: {
+    GlobalHeader,
+    GlobalFooter,
+    SideMenu
+  }
+}
+</script>
--- sakata-ryuichi-web/pages/about.vue
+++ sakata-ryuichi-web/pages/about.vue
@@ -1,27 +1,6 @@
 <template>
-  <section class="container">
-    This is about
-    <div>
-      <logo />
-      <h1 class="title">
-        sakata-ryuichi-web
-      </h1>
-      <h2 class="subtitle">
-        My prime Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+  <section>
+    This is About Page.
   </section>
 </template>
 
@@ -29,6 +8,7 @@
 import Logo from '~/components/Logo.vue'
 
 export default {
+  layout: 'twoColumn',
   components: {
     Logo
   }
