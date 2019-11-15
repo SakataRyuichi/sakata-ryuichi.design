778f5e2e6ab55bb0ce444495a4172ef8a647b3e9
SakataRyuichi <ryuichi.design@gmail.com>
2019-11-15 13:26:55 +0900

    kag test

--- sakata-ryuichi-web/dist/kagg/concierge/js/index.js
+++ sakata-ryuichi-web/dist/kagg/concierge/js/index.js
@@ -49,7 +49,7 @@ $(function(){
 });
 
 $(function(){
-  $('#datepicker').datepicker({
+  $('.datepicker').datepicker({
     monthNames: ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
   });
 
